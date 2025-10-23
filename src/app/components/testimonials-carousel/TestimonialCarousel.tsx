9aace1488c719d26586996c481c359a15f85f9ba
rickferreira <salviano.richard@academico.ifpb.edu.br>
2025-10-23 15:45:51 -0300

    feat: adiciona seção de depoimentos na tela principal

---
+++ src/app/components/testimonials-carousel/TestimonialCarousel.tsx
@@ -0,0 +1,69 @@
+"use client";
+
+import React from "react";
+import Slider from "react-slick";
+import TestimonialCard from "./TestimonialCard";
+import styles from "./TestimonialCarousel.module.css";
+
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+
+interface Testimonial {
+	id: string;
+	name: string;
+	content: string;
+	date: string;
+}
+
+interface TestimonialCarouselProps {
+	testimonials: Testimonial[];
+}
+
+export default function TestimonialCarousel({
+	testimonials,
+}: TestimonialCarouselProps) {
+	const settings = {
+		dots: true,
+		infinite: true,
+		speed: 500,
+		autoplay: true,
+		autoplaySpeed: 10000,
+		slidesToShow: 2,
+		slidesToScroll: 2,
+		arrows: true,
+		responsive: [
+			{
+				breakpoint: 992,
+				settings: {
+					slidesToShow: 1,
+					slidesToScroll: 1,
+				},
+			},
+		],
+	};
+
+	const getTestimonialTitle = (index: number) => {
+		const titles = ["Pai de Aluno", "Professor", "Ex-Aluno", "Voluntário"];
+		return titles[index % titles.length];
+	};
+
+	if (!testimonials || testimonials.length === 0) {
+		return null;
+	}
+
+	return (
+		<div className={styles.carouselContainer}>
+			<Slider {...settings}>
+				{testimonials.map((testimonial, index) => (
+					<div key={testimonial.id} className={styles.slideItem}>
+						<TestimonialCard
+							content={testimonial.content}
+							name={testimonial.name}
+							title={getTestimonialTitle(index)}
+						/>
+					</div>
+				))}
+			</Slider>
+		</div>
+	);
+}
